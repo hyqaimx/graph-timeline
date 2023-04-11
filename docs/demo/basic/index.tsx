555d0caac51f6b1bfbfb8ed7d130510890fc83cd
bjchenxn <bjchenxn@126.com>
2023-04-11 17:24:35 +0800

    doc: 更新demo

--- docs/demo/basic/index.tsx
+++ docs/demo/basic/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { GraphTimeline } from 'graph-timeline-v2';
 import demo1Data from './demo1';
-import './index.less'
+import './index.less';
 
 export default () => (
-  <GraphTimeline
-    {...demo1Data}
-    yAxis={{ width: 200 }}
-    style={{ height: 800, padding: 30 }}
-  />
+  <GraphTimeline {...demo1Data} yAxis={{ width: 80 }} style={{ height: 800, padding: 30 }} />
 );
